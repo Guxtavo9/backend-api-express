f32015fd2fe67fb0f1417cdb775df8ae36c7f21d
Guxtavo9 <pereira.gustavo2@aluno.ifsp.edu.br>
2024-02-09 15:51:26 -0300

    start of server settings

--- src/server.js
+++ src/server.js
@@ -1,1 +1,16 @@
-console.log("Hello World!")
+// const express = require('express')
+import express  from 'express'
+import { PORT, HOST } from './config.js'
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.post('/post', (req, res) => {
+  res.send('Hello World post!')
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${HOST}:${PORT}`)
+})
