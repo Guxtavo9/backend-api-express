ae94ee06d04337c0cf40e7799f4cc504f0d03748
Guxtavo9 <pereira.gustavo2@aluno.ifsp.edu.br>
2024-02-09 16:40:54 -0300

    Refactor server.js to use JSON response format

--- src/server.js
+++ src/server.js
@@ -1,16 +1,22 @@
 // const express = require('express')
-import express  from 'express'
-import { PORT, HOST } from './config.js'
-const app = express()
+import express from "express";
+import { PORT, HOST } from "./config.js";
+const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.json({
+    "message" : "Hello World!",
+  });
+});
 
-app.post('/post', (req, res) => {
-  res.send('Hello World post!')
-})
+app.post("/post", (req, res) => {
+  res.json({
+    "message" : "Hello World post!",
+  });
+});
+
+app.
 
 app.listen(PORT, () => {
-  console.log(`Server running on ${HOST}:${PORT}`)
-})
+  console.log(`Server running on ${HOST}:${PORT}`);
+});
