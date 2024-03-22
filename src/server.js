624a40ec5fb041093ec18e8c407547166954a31b
Guxtavo9 <pereira.gustavo2@aluno.ifsp.edu.br>
2024-03-22 14:53:22 -0300

    Add cors package and configure CORS settings

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 // const express = require('express')
 import express from "express";
+import cors from "cors";
 import { PORT, HOST } from "./config.js";
 import userRouter from "./routers/userRouter.js";
 import productRouter from "./routers/productRouter.js";
@@ -7,15 +8,19 @@ import logger from "./middlewares/logger.js";
 
 const app = express();
 
-app.use(express.json()) 
-app.use(logger)
+app.use(express.json());
+app.use(cors({
+  "origin": ["http://localhost:3000", 'http://localhost:8081/'],
+  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+}));
+app.use(logger);
 
-app.use('/user', userRouter);
-app.use('/product', productRouter);
+app.use("/user", userRouter);
+app.use("/product", productRouter);
 
 app.post("/post", (req, res) => {
   res.json({
-    "message" : "Hello World post!",
+    message: "Hello World post!",
   });
 });
 
